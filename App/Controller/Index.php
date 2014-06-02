292681b54c3af2f5fb6363eb15c120c7294d3e3c
Joseph Zhou <fangtee@qq.com>
2014-06-02 19:43:08 +0800

    deploy

--- App/Controller/Index.php
+++ App/Controller/Index.php
@@ -8,8 +8,10 @@ class Index{
     }
 }
 
-class Test{
+class Test111111{
     function get(){
+        var_dump(\Pt::$config);
+        exit;
         $db = PtDb::init("default");
         $res = $db->getOne("select * from user");
         var_dump($res);
